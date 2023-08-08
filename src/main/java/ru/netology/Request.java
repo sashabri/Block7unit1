5c2beb29e030b47def5e433b236927ac748256b2
Sasha Kom <xandria97@mail.ru>
2023-08-08 21:59:48 +0300

    final task 1 unit 2

--- src/main/java/ru/netology/Request.java
+++ src/main/java/ru/netology/Request.java
@@ -1,17 +1,26 @@
 package ru.netology;
 
-import java.net.http.HttpRequest;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Map;
 
 public class Request {
     private HttpMethod method;
     private Map<String,String> headers;
     private String body;
+    private String url;
+    private List<NameValuePair> queryParams;
+    private String path;
 
-    public Request(HttpMethod method, Map<String, String> headers, String body) {
+    public Request(HttpMethod method, Map<String, String> headers, String body, String url) {
         this.method = method;
         this.headers = headers;
         this.body = body;
+        this.url = url;
+        parseUrl();
     }
 
     public HttpMethod getMethod() {
@@ -25,4 +34,35 @@ public class Request {
     public String getBody() {
         return body;
     }
+
+    public List<NameValuePair> getQueryParams() {
+        return queryParams;
+    }
+
+    public String getQueryParam(String name) {
+        for (NameValuePair nameValuePair : queryParams) {
+            if(nameValuePair.getName().equals(name)) {
+                return nameValuePair.getValue();
+            }
+        }
+        return null;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+
+    private void parseUrl() {
+        String query = "";
+
+        String[] arr1 = url.split("/?");
+        path = arr1[0];
+        if(arr1.length > 1 && arr1[1] != null) {
+            String[] arr = arr1[1].split("/#");
+            query = arr[0];
+        }
+       queryParams = URLEncodedUtils.parse(query, StandardCharsets.UTF_8);
+    }
+
 }
